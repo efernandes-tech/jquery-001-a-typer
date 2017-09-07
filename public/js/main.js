b44c33030c58670ab837c08078252861dde9bc1b
edersonlrf <edersonluis.rf@gmail.com>
2017-09-07 19:55:50 -0300

    A5 verificando a digitacao

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ $(function(){
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
 
@@ -45,15 +46,34 @@ function inicializaCronometro() {
             }
         }, 1000);
     });
-}
+};
 
 function reiniciaJogo() {
     campo.attr("disabled", false);
-    //inicializando os campos
+    // Inicializando os campos.
     campo.val("");
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
     campo.toggleClass("campo-desativado");
+
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+};
+
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
 };
