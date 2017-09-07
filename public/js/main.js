940a627dd52bcf134b8fa1ffdb0d48ad9dd1db39
edersonlrf <edersonluis.rf@gmail.com>
2017-09-07 19:24:12 -0300

    A4 organizando o codigo

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,54 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var campo = $(".campo-digitacao");
 var tempoInicial = $("#tempo-digitacao").text();
 
-var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
-
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
 
-campo.one("focus", function() {
-    var cronometroID = setInterval(function() {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true);
-            // Para o "setInterval".
-            clearInterval(cronometroID);
-        }
-    }, 1000);
-});
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
 
-$("#botao-reiniciar").click(function(){
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+
+    campo.one("focus", function() {
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                // Para o "setInterval".
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
     campo.attr("disabled", false);
     //inicializando os campos
     campo.val("");
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
-
     $("#tempo-digitacao").text(tempoInicial);
-});
+    inicializaCronometro();
+};
