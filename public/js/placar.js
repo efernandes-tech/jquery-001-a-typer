c0f28c74b971eaf4c3415a24ef9c8f7b9f3648eb
edersonlrf <edersonluis.rf@gmail.com>
2017-09-07 21:22:23 -0300

    A6 organizando o codigo

---
+++ public/js/placar.js
@@ -0,0 +1,39 @@
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+
+    var usuario = "Seu-nome";
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+};
+
+function novaLinha(usuario, palavras) {
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").attr("href","#").addClass("botao-remover");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    // Ícone dentro do <a>.
+    link.append(icone);
+
+    // <a> dentro do <td>.
+    colunaRemover.append(link);
+
+    // Os três <td> dentro do <tr>.
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+};
+
+function removeLinha(event) {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+};
