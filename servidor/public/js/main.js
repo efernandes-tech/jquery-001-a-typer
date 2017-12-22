2dc86255f5e84d249fc3fa0b7e5a96392f4a3e1c
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-22 08:20:04 -0200

    2A2 frases diferentes para digitacao

---
+++ servidor/public/js/main.js
@@ -0,0 +1,84 @@
+var campo = $(".campo-digitacao");
+var tempoInicial = $("#tempo-digitacao").text();
+
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+};
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+
+    campo.one("focus", function() {
+        $("#botao-reiniciar").attr("disabled",true);
+
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                // Para o "setInterval".
+                clearInterval(cronometroID);
+                finalizaJogo();
+            }
+        }, 1000);
+    });
+};
+
+function finalizaJogo() {
+    campo.attr("disabled", true);
+    $("#botao-reiniciar").attr("disabled", false);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+};
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    // Inicializando os campos.
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+};
+
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+};
