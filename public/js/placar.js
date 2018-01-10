e28dc8ba55eb5437551f335310b2929c8614a7ce
edersonlrf <edersonluis.rf@gmail.com>
2018-01-09 22:09:45 -0200

    2A5 post com jquery

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-sync").click(sincronizaPlacar);
+
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
 
@@ -56,4 +58,26 @@ function scrollPlacar() {
     $("body").animate({
         scrollTop: posicaoPlacar + "px"
     }, 1000);
+};
+
+function sincronizaPlacar(){
+    var placar = [];
+    var linhas = $("tbody>tr");
+    linhas.each(function(){
+        var usuario = $(this).find("td:nth-child(1)").text();
+        var palavras = $(this).find("td:nth-child(2)").text();
+
+        var score = {
+            usuario: usuario,
+            pontos: palavras
+        };
+
+        placar.push(score); // Guardando o score no array.
+    });
+    var dados = {
+        placar: placar
+    };
+    $.post("http://localhost:3000/placar", dados, function(){
+        console.log("Placar sincronizado com sucesso");
+    });
 };
